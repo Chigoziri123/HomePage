f0216a580ec0cb76dd505e743d85eb0dd2a6b925
Samuel~ <chigozirianuna@gmail.com>
2023-09-29 14:38:45 +0100

    Worked on the Testimonials section

--- src/components/Joinus.jsx
+++ src/components/Joinus.jsx
@@ -33,21 +33,21 @@ const Joinus = () => {
             
                 className='w-full lg:w-3/4'>
                <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 items-start md:gap-12 gap-8'>
-                    <div className='bg-[rgba(255, 255, 255, 0.04)] rounded-[35px] h-96 shadow-3xl 9-8 flex items-center justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer'>
+                    <div className='bg-[rgba(255, 255, 255, 0.04)] rounded-[35px] h-96 shadow-3xl flex items-center justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer'>
                         <div>
                             <img src={purple1} className='h-20 w-20' alt='number 1 in purple color'/>
                             <h5 className='text-2xl font-semibold text-primary px-5 text-center mt-5'>Register on the website by providing necessary information.</h5>
                         </div>
                     </div> 
 
-                    <div className='bg-[rgba(255, 255, 255, 0.04)] rounded-[35px] h-96 shadow-3xl 9-8 flex items-center justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer md:mt-16'>
+                    <div className='bg-[rgba(255, 255, 255, 0.04)] rounded-[35px] h-96 shadow-3xl flex items-center justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer md:mt-16'>
                         <div>
                             <img src={purple2} className='h-20 w-20' alt='number 2 in purple color'/>
                             <h5 className='text-2xl font-semibold text-primary px-5 text-center mt-5'>Click on the link sent to your email to verify your account.</h5>
                         </div>
                     </div>
                     
-                    <div className='bg-[rgba(255, 255, 255, 0.04)] rounded-[35px] h-96 shadow-3xl 9-8 flex items-center justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer'>
+                    <div className='bg-[rgba(255, 255, 255, 0.04)] rounded-[35px] h-96 shadow-3xl flex items-center justify-center hover:-translate-y-4 transition-all duration-300 cursor-pointer'>
                         <div>
                             <img src={purple3} className='h-20 w-20' alt='number 3 in purple color'/>
                             <h5 className='text-2xl font-semibold text-primary px-5 text-center mt-5'>Log in to your account to enjoy our amazing services!</h5>
